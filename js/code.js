b393ddf479ebfdabf35bc19426f38c5d559dfb74
dementedapple5 <danihabana96@gmail.com>
2016-05-04 16:22:49 +0200

    cambios - 4/5/2016

--- js/code.js
+++ js/code.js
@@ -1,14 +1,17 @@
-var argLs = 'Argumentos:\n -a--> No ignora archivos ocultos\n -l--> Lista el contenido con informacion extra\n '
-argLs.replace('\n', '<br />')
+var demo_top = "media/videos/demo_top.webm";
+var demo_ifconfig = "media/videos/demo_ifconfig.webm";
+var demo_ls = "media/videos/demo_ls.webm";
+
 new Vue({
   el: 'body',
   data:{
     message: '',
 
     comandos:[
-      {nombre:'cd', descripcion:'Descripcion: Cambiar de carpetas', flags:''},
-      {nombre:'yum', descripcion:'Descripcion: Instalar paquetes', flags:''},
-      {nombre:'ls', descripcion:'Descripcion: listar contenido de un directorio', arg:argLs}
-    ]
+      {nombre:'cat', descripcion:'Funcion: Nos permite visualizar el contenido de un archivo de texto sin la necesidad de un editor', demos:'' },
+      {nombre:'ifconfig', descripcion:'Funcion: Verificar estado de la red', demos:demo_ifconfig},
+      {nombre:'ls', descripcion:'Funcion: permite listar el contenido de un directorio o fichero', demos:demo_ls},
+      {nombre:'top', descripcion:'Funcion: Muestra los procesos de linux', demos:demo_top},
+    ],
   },
 })
