53f8922a37c74cc4f7340ddb9bf0c9caaab9e90d
dementedapple5 <danihabana96@gmail.com>
2016-05-09 13:34:33 +0200

    ultimos cambios 9/5/2016

--- js/code.js
+++ js/code.js
@@ -2,11 +2,17 @@ var demo_top = "media/videos/demo_top.webm";
 var demo_ifconfig = "media/videos/demo_ifconfig.webm";
 var demo_ls = "media/videos/demo_ls.webm";
 
+Vue.transition('animacion',{
+  enterClass: 'fadeInDown',
+  leaveClass: 'fadeOutUp',
+});
+
 new Vue({
   el: 'body',
   data:{
     message: '',
-
+    mostrar_linux: false,
+    mostrar_GNU: false,
     comandos:[
       {nombre:'cat', descripcion:'Funcion: Nos permite visualizar el contenido de un archivo de texto sin la necesidad de un editor', demos:'' },
       {nombre:'ifconfig', descripcion:'Funcion: Verificar estado de la red', demos:demo_ifconfig},
