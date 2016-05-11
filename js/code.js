938ad0922d6c438d1440a161db2a558e96a17b0a
h2jie <h2jie09@gmail.com>
2016-05-11 17:48:46 +0200

    noticia2

---
+++ js/code.js
@@ -0,0 +1,28 @@
+var demo_top = "media/videos/demo_top.webm";
+var demo_ifconfig = "media/videos/demo_ifconfig.webm";
+var demo_ls = "media/videos/demo_ls.webm";
+
+Vue.transition('animacion',{
+  enterClass: 'fadeInDown',
+  leaveClass: 'fadeOutUp',
+});
+Vue.transition('animacion2',{
+  enterClass: 'bounceInDown',
+  leaveClass: 'fadeOutUp',
+});
+
+new Vue({
+  el: 'body',
+  data:{
+    message: '',
+    mostrar_linux: false,
+    mostrar_GNU: false,
+    mostrar_mensaje: false,
+    comandos:[
+      {nombre:'cat', descripcion:'Funcion: Nos permite visualizar el contenido de un archivo de texto sin la necesidad de un editor', demos:'' },
+      {nombre:'ifconfig', descripcion:'Funcion: Verificar estado de la red', demos:demo_ifconfig},
+      {nombre:'ls', descripcion:'Funcion: permite listar el contenido de un directorio o fichero', demos:demo_ls},
+      {nombre:'top', descripcion:'Funcion: Muestra los procesos de linux', demos:demo_top},
+    ],
+  },
+})
